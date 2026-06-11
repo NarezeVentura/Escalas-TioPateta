f3dcae4c798cbcc3456c6bfb1d790dc8bd93fd01
narezeventura <emaildovinicius08@gmail.com>
2026-06-11 10:41:25 -0300

    adição da página de perfil e atribuição da navegação entre páginas

--- front end/src/dashboard.jsx
+++ front end/src/dashboard.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
+
+import Escalas from "./Escalas";
+import Ferramentas from "./Ferramentas";
+import Reservas from "./Reservas";
+import Perfil from "./Perfil";
+
 export default function Dashboard({ user, onLogout }) {
+  const [pagina, setPagina] = useState("home");
+
   return (
     <div className="container">
       <div className="card">
         <div className="logo-placeholder">🦆</div>
         <h1>Equipe Tio Pateta</h1>
-        <p className="subtitle">Olá, {user?.nome || 'usuário'} 👋</p>
+        <p className="subtitle">Olá, {user?.nome || "usuário"} 👋</p>
 
+        {/* MENU */}
         <div className="buttons">
-          <button>📅 Escalas</button>
-          <button>🛠 Ferramentas</button>
-          <button>📋 Reservas</button>
-          <button>👤 Perfil</button>
+          <button onClick={() => setPagina("escalas")}>📅 Escalas</button>
+          <button onClick={() => setPagina("ferramentas")}>🛠 Ferramentas</button>
+          <button onClick={() => setPagina("reservas")}>📋 Reservas</button>
+          <button onClick={() => setPagina("perfil")}>👤 Perfil</button>
         </div>
 
-        <button className="btn-link" onClick={onLogout}>Sair</button>
+        <button className="btn-link" onClick={onLogout}>
+          Sair
+        </button>
       </div>
+
+      {/* NAVEGAÇÃO ENTRE PÁGINAS */}
+      {pagina === "escalas" && <Escalas />}
+      {pagina === "ferramentas" && <Ferramentas />}
+      {pagina === "reservas" && <Reservas />}
+      {pagina === "perfil" && <Perfil />}
     </div>
   );
 }
